8c0f665654850b3ade6f31a586384c47ed0239f6
Kris Dorohina <sun.08.90@mail.ru>
2025-03-20 12:34:10 +0300

    Добавил auth_data.py в .gitignore и удалил его из репозитория

--- auth_data.py
+++
@@ -1,8 +0,0 @@
-username = 'aleksandr.dorohin'
-password = '375333273338d'
-
-username_kris = 'kris.dorohina'
-password_kris = 'Rhbcnbyf04101990'
-
-username_beauty = 'beauty_for_minsk'
-password_beauty = 'Rhbcnbyf0410$'
